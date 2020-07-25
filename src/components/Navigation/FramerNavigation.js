6a10b9ac7b81212bc3d2447b6350b9c27bb43230
Devin Efendy <devinefendy@Devins-MacBook-Pro.local>
2020-07-24 22:56:03 -0500

    Remake sidebar template in Framer Motion

--- src/components/Navigation/FramerNavigation.js
+++ src/components/Navigation/FramerNavigation.js
@@ -1,37 +1,23 @@
 import * as React from "react"
-import { useRef } from "react"
 import { motion, useCycle } from "framer-motion"
-import styled from "styled-components"
+// import styled from "styled-components"
 import MenuToggle from "./MenuToggle"
+import NavigationSidebar from "./NavigationSidebar"
 
-const MotionNav = styled(motion.nav)``
-
-const Bg = styled(motion.div)`
-  position: absolute;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: 300px;
-  background: blue;
-`
-
-const sideBar = {
-  open: {},
-  closed: {},
-}
 
 const FramerNavigation = () => {
   const [isOpen, toggleOpen] = useCycle(false, true)
 
   return (
-    <MotionNav initial={false} animate={isOpen ? "open" : "closed"}>
-      {/* <Bg className="background" variants={sideBar} /> */}
+    <motion.nav initial={false} animate={isOpen ? "open" : "closed"}>
       <MenuToggle
         toggle={() => {
           toggleOpen()
         }}
       />
-    </MotionNav>
+      <NavigationSidebar/>
+      
+    </motion.nav>
   )
 }
 
--- src/components/Navigation/MenuToggle.js
+++ src/components/Navigation/MenuToggle.js
@@ -1,31 +1,37 @@
 import * as React from "react"
-import { useRef } from "react"
-import { motion, useCycle } from "framer-motion"
+import { motion } from "framer-motion"
 import styled from "styled-components"
 import Device from "../../utils/Device"
+
+// const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"]
 
 const ToggleButton = styled(motion.button)`
   position: fixed;
   height: 50px;
   width: 50px;
 
-  top: 2rem;
-  left: 1rem;
+  top: 1.5rem;
+  left: 1.5rem;
 
   z-index: 10;
 
-  border-radius: 10px;
+  border-radius: 25%;
   border: none;
   outline: none;
-  background: linear-gradient(to bottom left, #feac5e, #c779d0, #4bc0c8);
+  cursor: pointer;
 
-  box-shadow: 2px 2px 3px #999;
+  background: linear-gradient(to bottom right, #ff008c 0%, #d309e1 100%);
+
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
 
   padding: 3px;
 
   @media ${Device.min.Tablet} {
     height: 60px;
     width: 60px;
+
+    top: 2rem;
+    left: 2rem;
   }
 
   @media ${Device.min.LaptopL} {
@@ -33,7 +39,7 @@ const ToggleButton = styled(motion.button)`
     width: 70px;
 
     top: 3rem;
-    left: 2rem;
+    left: 3rem;
   }
 `
 
@@ -49,7 +55,8 @@ const Path = props => (
   <motion.path
     fill="transparent"
     strokeWidth="5"
-    stroke="hsl(0, 0%, 18%)"
+    // stroke="hsl(0, 0%, 18%)"
+    stroke="white"
     strokeLinecap="round"
     {...props}
   />
@@ -63,13 +70,26 @@ const MenuToggle = ({ toggle }) => {
       onClick={toggle}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.95 }}
+      variants={{
+        closed: {
+          boxShadow: "0px 3px 15px rgba(0,0,0,0.2)",
+          background:
+            "linear-gradient(to bottom right, #FF008C 0%, #D309E1 100%)",
+        },
+        open: {
+          boxShadow: "none",
+          background:
+            "linear-gradient(to bottom right, rgb(0,0,0,0) 0%, rgb(0,0,0,0) 100%)",
+        },
+      }}
     >
       <Svg>
         <Path
           variants={{
-            closed: { d: "M 5 7.5 L 30 7.5" },
-            open: { d: "M 5 30 L 30 5" },
+            closed: { d: "M 5 7.5 L 30 7.5", stroke: "white" },
+            open: { d: "M 5 30 L 30 5", stroke: "black" },
           }}
+          transition={{}}
         />
         <Path
           d="M 5 17.5 L 30 17.5"
@@ -81,8 +101,8 @@ const MenuToggle = ({ toggle }) => {
         />
         <Path
           variants={{
-            closed: { d: "M 5 27.5 L 30 27.5" },
-            open: { d: "M 5 5 L 30 30" },
+            closed: { d: "M 5 27.5 L 30 27.5", stroke: "white" },
+            open: { d: "M 5 5 L 30 30", stroke: "black" },
           }}
         />
       </Svg>
