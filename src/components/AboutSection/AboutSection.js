442127361df2c0d04dd9d2e0b790df9aed2968c5
Devin Efendy <devinefendy.dev@gmail.com>
2020-08-07 18:06:13 -0500

    Change About Section layout to grid

--- src/components/AboutSection/AboutSection.js
+++ src/components/AboutSection/AboutSection.js
@@ -27,23 +27,48 @@ const SectionContainer = styled(motion.div)`
   flex-direction: column;
 
   align-items: center;
-  padding: 0 1rem;
+  padding: 0 2rem;
   width: 100%;
 
-  border: 1px solid red;
+  /* border: 1px solid red; */
+
+  @media ${Device.min.Tablet} {
+    padding: 0 4rem;
+  }
+
+  @media ${Device.min.Laptop} {
+    padding: 0 9rem;
+  }
 `
 
 const ResponsiveContainer = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
+  display: inline-grid;
+
+  align-items: center;
+  justify-items: center;
+  /* align-items: center; */
+
   width: 100%;
 
-  /* align-items: flex-start; */
+  grid-template-columns: auto;
+  /* grid-template-rows: auto auto; */
 
-  border: 1px solid white;
+  /* border: 1px solid white; */
 
   @media ${Device.min.Laptop} {
-    flex-direction: row;
+    grid-column-gap: 10px;
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @media (min-width: 1000) {
+    grid-column-gap: 10px;
+    grid-template-columns: repeat(5, 1fr);
+  }
+`
+
+const TextContainer = styled(motion.div)`
+  @media ${Device.min.Laptop} {
+    grid-column: 1 / 4;
   }
 `
 
@@ -61,21 +86,22 @@ const TitleContainer = styled(motion.div)`
 `
 
 const ImgContainer = styled(motion.div)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  /* width: 100%; */
+  width: 100%;
+  grid-column: 1 / 2;
+
+  @media ${Device.min.Laptop} {
+    grid-column: 4 / 6;
+  }
 `
 
 const ImgMobile = styled(motion.custom(Img))`
-  display: flex;
-  flex-direction: column;
-  width: 250px;
-  border-radius: 25px;
+  border-radius: 10px;
+  width: 100%;
 
-  @media ${Device.min.Tablet} {
-    width: 300px;
+  margin: auto;
+
+  @media ${Device.min.Laptop} {
+    width: 70%;
   }
 `
 const AboutHeading = styled(motion.h1)`
@@ -89,8 +115,8 @@ const AboutHeading = styled(motion.h1)`
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 
-  @media ${Device.min.Tablet} {
-    /* font-size: 2rem; */
+  @media ${Device.min.Laptop} {
+    max-width: 500px;
   }
 `
 
@@ -98,7 +124,7 @@ const KeyVal = styled(motion.span)``
 
 const AboutParagraph = styled(motion.p)`
   margin-bottom: 3rem;
-  max-width: 500px;
+  /* max-width: 500px; */
 
   color: rgba(255, 255, 255, 0.75);
 
@@ -144,8 +170,9 @@ const AboutSection = () => {
           <motion.h2>about me</motion.h2>
           {/* <Scribble style={{fontSize: "2rem"}}>About Me</Scribble> */}
         </TitleContainer>
+
         <ResponsiveContainer>
-          <motion.div>
+          <TextContainer>
             <Scribble>My strongest values </Scribble>
 
             <AboutHeading>
@@ -162,13 +189,9 @@ const AboutSection = () => {
               things tech. Especially, in area such as Software Engineering, Web
               Development, and UI/UX Design. But, I would be happy to explore
               other areas too!
-              <br />
-              <br />
-              Currently, I'm working at Bold Commerce where I learn a lot about
-              E-commerce, software engineering & agile practices, and also some
-              knowledge of DevOps.
             </AboutParagraph>
-          </motion.div>
+          </TextContainer>
+
           <ImgContainer>
             <ImgMobile fluid={sources} />
           </ImgContainer>
